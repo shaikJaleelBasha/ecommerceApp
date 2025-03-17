4146ec1063abea0b1e1cf02ec549e8398884cc89
shaikJaleelBasha <jaleelbashashaik18@gmail.com>
2025-03-17 23:34:07 +0530

    dependencies for the project is added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,13 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    // Dagger Hilt dependency injection
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+
+    // Cross-platform data transfer
+    kotlin("plugin.serialization") version "2.1.10"
 }
 
 android {
@@ -40,7 +47,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -56,4 +62,33 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Dagger Hilt
+    implementation("com.google.dagger:hilt-android:2.50")
+    kapt("com.google.dagger:hilt-android-compiler:2.50")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    // Coil (Image loading)
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
+    // Navigation Compose
+    implementation("androidx.navigation:navigation-compose:2.8.0-beta06")
+
+    // Kotlin Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
+    // Accompanist Pager
+    implementation("com.google.accompanist:accompanist-pager:0.28.0")
+    implementation("com.google.accompanist:accompanist-pager-indicators:0.28.0")
+
+    // Razorpay (Payment Gateway)
+    implementation("com.razorpay:checkout:1.6.40")
+
+    //custom bottom nev bar
+implementation("com.canopas.compose-animated-navigationbar:bottombar:1.0.1")
+}
+
+// Hilt configuration
+kapt {
+    correctErrorTypes = true
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,11 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id ("com.google.dagger.hilt.android") version "2.50" apply false // Add this line
+}
+
+buildscript {
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.50") //add this line
+    }
 }
